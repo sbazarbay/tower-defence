0940feb1b20cb4cb54430337f053f647d6653020
Cheena <c.bazarbai@gmail.com>
2022-03-03 22:54:39 +0600

    Added WaveSpawner script, also adjusted spawn times and enemy speed, and added timer display;

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float speed = 10f;
+    public float speed = 5f;
 
     private Transform target;
     private int waypointIndex = 0;
---
+++ Assets/WaveSpawner.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+    public Text waveCountdownTimer;
+    public float timeBetweenWaves = 55f;
+    private float countdown = 30f;
+
+    private int waveSize = 19;
+
+    void Update()
+    {
+        waveCountdownTimer.text = Mathf.Floor(countdown).ToString();
+
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+            timeBetweenWaves += 2;
+        }
+        countdown -= Time.deltaTime;
+
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveSize++;
+
+        for (int i = 0; i < waveSize; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(1f);
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
